90d347e56401dfc4654b2d8ab911a1a8a1939eb7
hawonlee <gracehawonlee@gmail.com>
2024-02-19 10:36:44 -0500

    a

--- EmptyViewsTemplate/app/src/main/java/com/example/emptyviewstemplate/HitsActivity.java
+++ EmptyViewsTemplate/app/src/main/java/com/example/emptyviewstemplate/HitsActivity.java
@@ -1,63 +1,55 @@
 package com.example.emptyviewstemplate;
 
-import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
-public class HitsActivity extends AppCompatActivity {
+public class HitsActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private Integer hits;
     private TextView msg;
     private SharedPreferences myPrefs;
 
-
-    private final View.OnClickListener bigListener = new View.OnClickListener()  {
-        public void onClick(View v)  {
-            // do stuff
-            hits += 10;
-            msg.setText(hits + "");
-        }
-    };
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hits);
 
-        Intent intent = getIntent();
+        Spinner spinner = findViewById(R.id.spinner1);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.times_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        //spinner.setOnItemSelectedListener(this);
 
-        myPrefs = getApplicationContext().getSharedPreferences(String.valueOf(R.string.context_prefs), Context.MODE_PRIVATE);
-        hits = myPrefs.getInt("hitsValue", -1);
 
-        msg = (TextView) findViewById(R.id.msg);
-        msg.setText(hits + "");
-
-        Button little = (Button) findViewById(R.id.btn_sm);
-        little.setOnClickListener(new View.OnClickListener() {
-                                      public void onClick(View v) {// Do something in response to button click 
-                                          hits += 1;
-                                          msg.setText(hits + "");
-                                      }
-                                  }
-        );
-
-        Button big = (Button) findViewById(R.id.btn_lg);
-        big.setOnClickListener(bigListener);
     }
 
     @Override
-    protected void onPause() {
-        SharedPreferences.Editor peditor = myPrefs.edit();
-        peditor.putInt("hitsValue", hits);
-        peditor.apply();   // TO SAVE CHANGES
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        if (text.equals("America/New_York")) {
 
-        super.onPause();
+        } else if (text.equals("Europe/Berlin")) {
+
+        }else if (text.equals("Asia/Tokyo")) {
+
+        }else if (text.equals("Asia/Singapore")) {
+
+        }else if (text.equals("Australia/Canberra")) {
+
+        }else if (text.equals("India/Kolkata")) {
+
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
+
